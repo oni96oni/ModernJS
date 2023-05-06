cc936134107de240943aa371e87b26753da4890e
rainbow2774 <rainbow2774@gmail.com>
2023-05-06 20:30:13 +0900

    first

--- ex1/index.js
+++ ex1/index.js
@@ -1,3 +1,15 @@
 const arr = [1, 2, 3];
 
 arr.forEach(alert);
+
+var userId = 1;
+var userName = 'Cha';
+
+var user = { id: 1, name: 'Cha'};
+
+var users = [
+    { id: 1, name: 'Cha'},
+    { id: 2, name: 'Kim'}
+]
+
+var result = 10 + 20;
